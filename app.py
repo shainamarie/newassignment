75053cae3f8ceb747e2eb839805308b77018bf96
shainamarie <sweetmarie980@gmail.com>
2019-02-21 18:18:01 +0800

    updated

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import requests, json
-from flask import Flask, render_template, jsonify
+from flask import Flask, render_template, jsonify, request
 import textwrap
 
 
@@ -12,7 +12,13 @@ app.secret_key = 'gwapakooo'
 
 @app.route('/')
 def index():
-	return render_template('home.html')
+	url = 'https://musicdemons.com/api/v1/song'
+	headers = {}
+	response = requests.request('GET', url, headers = headers, allow_redirects=False)
+	song_dict = json.loads(response.text)
+	print(song_dict)
+	
+	return render_template('home.html', songs=song_dict)
 
 @app.route('/artists', methods=['GET'])
 def artists():
@@ -48,6 +54,47 @@ def artist_songs(artist_id, artist_name):
 	song_dict = json.loads(response.text)
 
 	return render_template('artist_songs.html', songs=song_dict, artist=artist)
+
+
+# SEARCH API FUNCTION IS NOT WORKING
+# API BROKEN
+
+# @app.route('/search', methods=['POST'])
+# def search():
+# 	# keywords = request.form['key_word']
+# 	# print(keywords)
+# 	# url1 = 'https://musicdemons.com/api/v1/song/organic-search/all together now farm'
+# 	# print(url1)
+# 	# headers1 = {}
+# 	# response1 = requests.request('GET', url1, headers = headers1, allow_redirects=False)
+	
+# 	# song_dict1 = json.loads(response1.text)
+
+# 	url2 = 'https://musicdemons.com/api/v1/person/organic-search/john'
+# 	headers2 = {}
+# 	response2 = requests.request('GET', url2, headers = headers2, allow_redirects=False)
+# 	print(response2)
+# 	song_dict2 = json.loads(response2.text)
+# 	print(song_dict2)
+
+# 	# url3 = 'https://musicdemons.com/api/v1/song/organic-search/{}'.format(keywords)
+# 	# headers3 = {
+#  #  		'with': 'lyrics'
+# 	# }
+# 	# response3 = requests.request('GET', url3, headers = headers3, allow_redirects=False)
+# 	# print(url3)
+# 	# song_dict3 = json.loads(response3.text)
+
+# 	# url4 = 'https://musicdemons.com/api/v1/artist/organic-search/{}'.format(keywords)
+# 	# headers4 = {}
+# 	# response4 = requests.request('GET', url4, headers = headers4, allow_redirects=False)
+# 	# song_dict4 = json.loads(response4.text)
+
+
+# 	return render_template('result.html', song2=song_dict2)
+
+
+
 
 
 @app.route('/lyrics/<song_title>/<song_id>', methods=['GET'])
